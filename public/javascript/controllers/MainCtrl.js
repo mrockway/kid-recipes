f9253a47cd76c9a68d6dd51e9385443a4416ac5e
mrockway <mrockway@gmail.com>
2016-06-28 23:23:21 -0700

    troubleshoot homepage remove angular cards

--- public/javascript/controllers/MainCtrl.js
+++ public/javascript/controllers/MainCtrl.js
@@ -1,5 +1,5 @@
 angular.module('kidsFood')
-.controller("MainCtrl", ['$scope', '$http', '$mdBottomSheet', function($scope, $http, $mdBottomSheet){
+.controller("HeaderCtrl", ['$mdBottomSheet', function($mdBottomSheet){
 
 	$scope.showMobileMenu = function() {
 		$mdBottomSheet.show({
@@ -7,11 +7,5 @@ angular.module('kidsFood')
 			controller: 'MainCtrl'
 		});
 	};
-
-	$scope.cardInfo = [{title: 'Create', content: 'Create and store your recipes online and bring them with you everywhere you go.'},
-										 {title: 'Discover', content:'Get ideas from other members to try out with your own family'},
-										 {title: 'Share', content: 'Share your recipes with other members or keep them all to yourself.'}
-										];
-
 }]);
 
